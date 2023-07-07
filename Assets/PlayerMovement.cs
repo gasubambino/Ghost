d4f38089cb6917919d1ebd80a61c56351983d876
gasubambino <fernandonunesferreira12@gmail.com>
2023-07-07 01:57:26 -0300

    .

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float xMovement = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(xMovement * speed, rb.velocity.y);
+        if (xMovement == 1)
+        {
+            Vector3 rotator = new Vector3(transform.rotation.x,0,transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator); 
+        }
+        if (xMovement == -1)
+        {
+            Vector3 rotator = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+        }
     }
 }
